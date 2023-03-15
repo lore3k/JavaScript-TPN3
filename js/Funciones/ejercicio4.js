dbf763751359d6bcc9db26d4cdaea47fa8cdfb0a
Lore Ginard <lorenaginard@hotmail.com>
2023-03-15 12:49:37 -0300

    Ejercicio 4 funciones terminado

--- js/Funciones/ejercicio4.js
+++ js/Funciones/ejercicio4.js
@@ -5,3 +5,18 @@ número ingresado por pantalla, la creación de la tabla debe ser
 realizada con una función y mostrar solo los resultados del 1 al 10 
 del número elegido por el usuario.
  */
+
+let numero = prompt("Ingrese un número");
+
+tablas(numero);
+
+function tablas(numero){
+    document.write('<h2> Tabla de multiplicar del número '+numero+'</h2>')
+    document.write('<ul>');
+    for(let i=1; i<=10;i++){
+        document.write(`<li>`);
+        document.write(numero+"*"+i+" = "+(numero*i));
+        document.write(`</li>`);
+    }
+    document.write('</ul>');
+}
